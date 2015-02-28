de6fa9bdc505466ec5d9e1dc23fecab8406cde5d
jixiuf <jixiuf@gmail.com>
2015-03-01 01:39:22 +0800

    handleuser

--- hello.go
+++ hello.go
@@ -3,6 +3,7 @@ package hello
 import (
 	"appengine"
 	"appengine/urlfetch"
+	"appengine/user"
 	"fmt"
 	"net/http"
 )
@@ -11,7 +12,34 @@ func init() {
 	http.HandleFunc("/index", handlerIndex)
 	http.HandleFunc("/helloworld", handlerHelloWorld)
 	http.HandleFunc("/fetch", handlerFetchUrl)
+	http.HandleFunc("/user", handlerUser)
 
+}
+func handlerIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<a href='helloworld'>helloworld</a><br/>")
+	fmt.Fprint(w, "<a href='fetch'>fetch google url</a>")
+	fmt.Fprint(w, "<a href='user'>say hello to appengine.user</a>")
+}
+
+func handlerHelloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, world!")
+}
+
+func handlerUser(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprint(w, "Hello, User:%v!", u)
 }
 
 func handlerFetchUrl(w http.ResponseWriter, r *http.Request) {
@@ -23,12 +51,4 @@ func handlerFetchUrl(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Fprintf(w, "HTTP GET returned status %v", resp.Status)
-}
-
-func handlerHelloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!")
-}
-func handlerIndex(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<a href='helloworld'>helloworld</a><br/>")
-	fmt.Fprint(w, "<a href='fetch'>fetch google url</a>")
 }
